9e4eda41e9fd9a1f415701fc6a4c04f981542e84
parth2374 <143696865+parth2374@users.noreply.github.com>
2025-06-20 21:21:52 +0530

    Update Product.js

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,3 +1,24 @@
+// const mongoose = require("mongoose");
+
+// const ProductSchema = new mongoose.Schema(
+//   {
+//     image: String,
+//     title: String,
+//     origin: String,
+//     certificate: String,
+//     price: Number,
+//     weight: Number,
+//     sku: Number,
+//     shape: String,
+//     video: String,
+//     frontSide: String,
+//     backSide: String
+//   },
+//   { timestamps: true }
+// );
+
+// module.exports = mongoose.model("Product", ProductSchema);
+
 const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema(
@@ -12,7 +33,13 @@ const ProductSchema = new mongoose.Schema(
     shape: String,
     video: String,
     frontSide: String,
-    backSide: String
+    backSide: String,
+
+    // new field: whether to show this product
+    isListed: {
+      type: Boolean,
+      default: true,
+    },
   },
   { timestamps: true }
 );
