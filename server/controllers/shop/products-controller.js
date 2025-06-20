663542a7489c01cfc9fc50c4b0f766e3cb41884e
parth2374 <143696865+parth2374@users.noreply.github.com>
2025-06-20 21:23:10 +0530

    Update products-controller.js

--- server/controllers/shop/products-controller.js
+++ server/controllers/shop/products-controller.js
@@ -61,7 +61,8 @@ const getFilteredProducts = async (req, res) => {
         break;
     }
 
-    const products = await Product.find(filters).sort(sort);
+    // const products = await Product.find(filters).sort(sort);
+    const products = await Product.find({ ...filters, isListed: true }).sort(sort);
 
     res.status(200).json({
       success: true,
